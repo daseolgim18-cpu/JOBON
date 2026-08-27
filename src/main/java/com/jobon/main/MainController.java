b8e0d6d06dec5d07870a21afe6946c67923b1f1a
daseol96 <daseolgim18@gmail.com>
2026-08-27 13:54:53 +0900

    refactor: 메인 컨트롤러 패키지 구조 변경

--- src/main/java/com/jobon/main/MainController.java
+++ src/main/java/com/jobon/main/MainController.java
@@ -1,4 +1,4 @@
-package com.jobon.controller;
+package com.jobon.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +11,4 @@ public class MainController {
         return "main";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "member/login";
-    }
 }
